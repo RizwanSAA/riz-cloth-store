05f4f8b3bac09aed7a62efd814923003f78af99d
RizwanSAA <sabaarizwan@outlook.com>
2020-02-17 22:01:53 +0530

    Redux setup and redirect after sign-in

--- src/App.js
+++ src/App.js
@@ -1,27 +1,33 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
+import { connect } from "react-redux";
 import "./App.css";
 
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
-import { auth } from "./firebase/firebase.utils";
+import { setCurrentUser } from "./redux/user/user.actions";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 
 class App extends React.Component {
-  constructor() {
-    super();
-
-    this.state = {
-      currentUser: null
-    };
-  }
   unsubscribeFromAuth = null;
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user });
+    console.log(this.props);
+    const { setCurrentUser } = this.props;
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
 
-      console.log(this.state.currentUser);
+        userRef.onSnapshot(snapShot => {
+          setCurrentUser({
+            id: snapShot.id,
+            ...snapShot.data()
+          });
+        });
+      } else {
+        setCurrentUser(userAuth);
+      }
     });
   }
 
@@ -32,15 +38,29 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Header currentUser={this.state.currentUser} />
+        <Header />
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
-          <Route path="/signin" component={SignInAndSignUpPage} />
+          <Route
+            exact
+            path="/signin"
+            render={() => (this.props.currentUser ? <Redirect to="/" /> : <SignInAndSignUpPage />)}
+          />
         </Switch>
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser
+});
+
+const mapDispatchToProps = dispatch => ({
+  setCurrentUser: user => {
+    dispatch(setCurrentUser(user));
+  }
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,14 +3,39 @@ import "firebase/firestore";
 import "firebase/auth";
 
 const config = {
-  apiKey: "AIzaSyBe0N3tms33FDHJugukMnIlyiVRH6i3YGA",
-  authDomain: "cloth-store-db-1227.firebaseapp.com",
-  databaseURL: "https://cloth-store-db-1227.firebaseio.com",
-  projectId: "cloth-store-db-1227",
-  storageBucket: "cloth-store-db-1227.appspot.com",
-  messagingSenderId: "372919935006",
-  appId: "1:372919935006:web:8d7a0425b3adc248e5ce0f",
-  measurementId: "G-B74180B480"
+  apiKey: "AIzaSyACJHtwwq9uRqmFkrcabUtMaYN_PKXLfAg",
+  authDomain: "my-store-87e34.firebaseapp.com",
+  databaseURL: "https://my-store-87e34.firebaseio.com",
+  projectId: "my-store-87e34",
+  storageBucket: "my-store-87e34.appspot.com",
+  messagingSenderId: "227389685967",
+  appId: "1:227389685967:web:4757e8511b9900f14e37ba"
+};
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`/users/${userAuth.uid}`);
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      });
+    } catch (error) {
+      console.log("error creating user", error.message);
+    }
+  }
+  return userRef;
+
+  // console.log(snapShot);
 };
 
 firebase.initializeApp(config);
