fef65073634cf93f72738659760dd1ff46ea1fda
RizwanSAA <sabaarizwan@outlook.com>
2020-06-13 20:56:13 +0530

    added redux feature

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -5,6 +5,11 @@ import { connect } from "react-redux";
 import { ReactComponent as Logo } from "../../assets/crown.svg";
 import CartIcon from "../cart-icon/cart-icon.component";
 import CartDropdown from "../cart-dropdown/cart-dropdown.component";
+
+import { createStructuredSelector } from "reselect";
+import { selectCurrentUser } from "../../redux/user/user.selector";
+import { selectHidden } from "../../redux/cart/cart.selector";
+
 import { auth } from "../../firebase/firebase.utils";
 
 import "./header.styles.scss";
@@ -36,9 +41,16 @@ const Header = ({ currentUser, hidden }) => (
   </div>
 );
 
-const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
-  currentUser,
-  hidden
+// one way of passing props to component without caching
+// const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+//   currentUser,
+//   hidden
+// });
+
+// another way of passing props to component with caching or memoization
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser,
+  hidden: selectHidden
 });
 
 export default connect(mapStateToProps)(Header);
