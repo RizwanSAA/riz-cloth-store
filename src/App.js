701422335887bf5a0c551711e4bf3a6952af294f
RizwanSAA <sabaarizwan@outlook.com>
2020-07-04 20:04:18 +0530

    added add and remove item functionality

--- src/App.js
+++ src/App.js
@@ -5,8 +5,11 @@ import "./App.css";
 
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
+import CheckoutPage from "./pages/checkout/checkout.component";
+
 import Header from "./components/header/header.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
+
 import { setCurrentUser } from "./redux/user/user.actions";
 import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 
@@ -15,14 +18,14 @@ class App extends React.Component {
   componentDidMount() {
     console.log(this.props);
     const { setCurrentUser } = this.props;
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
 
-        userRef.onSnapshot(snapShot => {
+        userRef.onSnapshot((snapShot) => {
           setCurrentUser({
             id: snapShot.id,
-            ...snapShot.data()
+            ...snapShot.data(),
           });
         });
       } else {
@@ -42,6 +45,7 @@ class App extends React.Component {
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
+          <Route exact path="/checkout" component={CheckoutPage} />
           <Route
             exact
             path="/signin"
@@ -54,13 +58,11 @@ class App extends React.Component {
 }
 
 const mapStateToProps = ({ user }) => ({
-  currentUser: user.currentUser
+  currentUser: user.currentUser,
 });
 
-const mapDispatchToProps = dispatch => ({
-  setCurrentUser: user => {
-    dispatch(setCurrentUser(user));
-  }
+const mapDispatchToProps = (dispatch) => ({
+  setCurrentUser: (user) => dispatch(setCurrentUser(user)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/redux/cart/cart.selector.js
+++ src/redux/cart/cart.selector.js
@@ -1,11 +1,15 @@
 import { createSelector } from "reselect";
 
-const selectCart = state => state.cart;
+const selectCart = (state) => state.cart;
 
-export const selectCartItems = createSelector([selectCart], cart => cart.cartItems);
+export const selectCartItems = createSelector([selectCart], (cart) => cart.cartItems);
 
-export const selectCartItemCount = createSelector([selectCartItems], cartItems =>
+export const selectCartItemCount = createSelector([selectCartItems], (cartItems) =>
   cartItems.reduce((accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity, 0)
 );
 
-export const selectHidden = createSelector([selectCart], cart => cart.hidden);
+export const selectHidden = createSelector([selectCart], (cart) => cart.hidden);
+
+export const selectCartTotal = createSelector([selectCartItems], (cartItems) =>
+  cartItems.reduce((accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity * cartItem.price, 0)
+);
