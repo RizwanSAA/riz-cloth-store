88c635fd6f1221d9092a56de3dc36acc33f72028
RizwanSAA <sabaarizwan@outlook.com>
2020-10-07 23:46:59 +0530

    add comment in firebase util file

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -2,6 +2,7 @@ import firebase from "firebase/app";
 import "firebase/firestore";
 import "firebase/auth";
 
+// use config object from google's firebase project
 const config = {};
 
 export const createUserProfileDocument = async (userAuth, additionalData) => {
